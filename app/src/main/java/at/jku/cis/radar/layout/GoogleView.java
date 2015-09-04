160c42e17075bf5390251e2e2339134b9bc58aac
Markus <Markus.Weissenbek@gmx.at>
2015-09-04 14:33:15 +0200

    speeded up line intersection, calc for lines from top left to bottom right works

--- app/src/main/java/at/jku/cis/radar/layout/GoogleView.java
+++ app/src/main/java/at/jku/cis/radar/layout/GoogleView.java
@@ -3,41 +3,42 @@ package at.jku.cis.radar.layout;
 import android.content.Context;
 import android.graphics.Point;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.MotionEvent;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 
 public class GoogleView extends MapView {
     private GoogleMap googleMap;
-    private List<PolylineOptions> polyLines = new ArrayList<>();
+    private CopyOnWriteArrayList<PolylineOptions> polyLines = new CopyOnWriteArrayList<>();
     private PolylineOptions line = null;
     private PolylineOptions eraserLine = null;
     private boolean isErasing = false;
 
-    public GoogleView(Context context) {
-        super(context);
-    }
+    WindowManager wm;
 
     public GoogleView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
     }
 
-    public GoogleView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
 
     public void setMap(GoogleMap googleMap) {
         this.googleMap = googleMap;
         this.googleMap.setMyLocationEnabled(true);
         this.googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         this.googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+
     }
 
     @Override
@@ -47,28 +48,37 @@ public class GoogleView extends MapView {
             LatLng currentLatLng = googleMap.getProjection().fromScreenLocation(currentPosition);
             if (motionEvent.getToolType(0) == MotionEvent.TOOL_TYPE_STYLUS) {
                 if (isErasing) {
-                    if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                        eraserLine = new PolylineOptions();
-                    }
-                    eraserLine.add(currentLatLng);
-                    if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                       lineIntersected(eraserLine);
-                    }
+                    //doPainting(motionEvent, currentLatLng);
+                    doErasing(motionEvent, currentLatLng);
                 } else {
-                    if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                        line = new PolylineOptions();
-                    }
-                    line.add(currentLatLng);
-                    if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                        googleMap.addPolyline(line);
-                        polyLines.add(line);
-                    }
+                    doPainting(motionEvent, currentLatLng);
                 }
-            }else {
+            } else {
                 super.dispatchTouchEvent(motionEvent);
             }
         }
         return true;
+    }
+
+    private void doPainting(MotionEvent motionEvent, LatLng currentLatLng) {
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            line = new PolylineOptions();
+        }
+        line.add(currentLatLng);
+        if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+            googleMap.addPolyline(line);
+            polyLines.add(line);
+        }
+    }
+
+    private void doErasing(MotionEvent motionEvent, LatLng currentLatLng) {
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            eraserLine = new PolylineOptions();
+        }
+        eraserLine.add(currentLatLng);
+        if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+            lineIntersected(eraserLine);
+        }
     }
 
     public void setEraser() {
@@ -76,26 +86,41 @@ public class GoogleView extends MapView {
     }
 
     private boolean lineIntersected(PolylineOptions eraserLine) {
+        Projection projection = googleMap.getProjection();
         lineLoop:
         for (PolylineOptions line : polyLines) {
+
             Point prev = null;
             Point prevEraser = null;
             for (LatLng latLng : line.getPoints()) {
-                Point currentPoint = googleMap.getProjection().toScreenLocation(latLng);
+                Point currentPoint = projection.toScreenLocation(latLng);
+                if (!pointInScreen(currentPoint)) {
+                    continue;
+                }
                 for (LatLng eraserLatLng : eraserLine.getPoints()) {
-                    Point currentEraserPoint = googleMap.getProjection().toScreenLocation(eraserLatLng);
+                    Point currentEraserPoint = projection.toScreenLocation(eraserLatLng);
                     if (prev != null && prevEraser != null) {
-                        //Statement not finished!!!
-
-                        int differenceXLine = prev.x - currentPoint.x;
-                        int differenceYLine = prev.y - currentPoint.y;
-                        int differenceXEraser = prevEraser.x - currentEraserPoint.x;
-                        int differenceYEraser = prevEraser.y - currentEraserPoint.y;
-                        int dividend = prevEraser.x * differenceYLine - prevEraser.y * differenceXLine + prev.y * differenceXLine - prev.x * differenceYLine;
-
+                        int differenceXLine = Math.abs(prev.x - currentPoint.x);
+                        int differenceYLine = Math.abs(prev.y - currentPoint.y);
+                        int differenceXEraser = Math.abs(prevEraser.x - currentEraserPoint.x);
+                        int differenceYEraser = Math.abs(prevEraser.y - currentEraserPoint.y);
+                        int smallestXEraser = findSmallest(prevEraser.x, currentEraserPoint.x);
+                        int smallestYEraser = findSmallest(prevEraser.y, currentEraserPoint.y);
+                        int smallestYLine = findSmallest(prev.y, currentPoint.y);
+                        int smallestXLine = findSmallest(prev.x, currentPoint.x);
+                        int dividend = smallestXEraser * differenceYLine - smallestYEraser * differenceXLine + smallestYLine * differenceXLine - smallestXLine * differenceYLine;
                         int divisor = differenceYEraser * (differenceXLine) - differenceXEraser * differenceYLine;
-                        double factorY = dividend/ divisor;
+                        if (divisor == 0) {
+                            continue;
+                        }
+                        double factorY = (double) dividend / (double) divisor;
 
+                        double eraserLineEquation = smallestXEraser + differenceXEraser * factorY;
+                        double factorX = (eraserLineEquation - smallestXLine) / differenceXLine;
+                        if (checkFactor(factorX) && checkFactor(factorY)) {
+                            polyLines.remove(line);
+                            continue lineLoop;
+                        }
                     }
                     prevEraser = currentEraserPoint;
 
@@ -103,6 +128,37 @@ public class GoogleView extends MapView {
                 prev = currentPoint;
             }
         }
+        repaintMap();
         return false;
+    }
+
+    private int findSmallest(int i, int j) {
+        return (i < j) ? i : j;
+    }
+
+    private void repaintMap() {
+        googleMap.clear();
+        for (PolylineOptions line : polyLines) {
+            googleMap.addPolyline(line);
+        }
+    }
+
+    private boolean pointInScreen(Point p) {
+
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        if (p.x < 0 || p.y < 0) {
+            return false;
+        }
+        if (p.x > size.x || p.y > size.y) {
+            return false;
+        }
+        return true;
+    }
+
+
+    private boolean checkFactor(double factor) {
+        return (factor <= 1.0) && (factor >= 0.0);
     }
 }
