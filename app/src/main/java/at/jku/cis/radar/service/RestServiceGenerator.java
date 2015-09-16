23187b9d8956c8527eb2d16a442e96b7179fa916
Berthold <berthold.roiser@gmail.com>
2015-09-16 08:50:57 +0200

    RestClient

---
+++ app/src/main/java/at/jku/cis/radar/service/RestServiceGenerator.java
@@ -0,0 +1,16 @@
+package at.jku.cis.radar.service;
+
+import retrofit.RestAdapter;
+import retrofit.android.AndroidApacheClient;
+
+public class RestServiceGenerator {
+
+    private static final String BASE_URL = "http://localhost:8080/rest";
+
+    public static <S> S createService(Class<S> serviceClass) {
+        RestAdapter.Builder builder = new RestAdapter.Builder()
+                .setEndpoint(BASE_URL)
+                .setClient(new AndroidApacheClient());
+        return builder.build().create(serviceClass);
+    }
+}
