97a63ee6cb4cb3c3f46f53a77bab51161fa2cf02
Markus <Markus.Weissenbek@gmx.at>
2015-10-03 18:39:13 +0200

    union Mode will work now correctly * no more app crashes * no endless loops, when union will not work correctly consecutive times * no holes more allowed ==> so union with these structures work now correctly

--- app/src/main/java/at/jku/cis/radar/geometry/GeometryUtils.java
+++ app/src/main/java/at/jku/cis/radar/geometry/GeometryUtils.java
@@ -3,14 +3,18 @@ package at.jku.cis.radar.geometry;
 import android.graphics.Color;
 
 import com.google.maps.android.geojson.GeoJsonFeature;
+import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryCollection;
 import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LinearRing;
 import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.geom.Polygonal;
 import com.vividsolutions.jts.geom.TopologyException;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import at.jku.cis.radar.service.PolygonRepairerService;
 
@@ -19,6 +23,7 @@ public class GeometryUtils {
     private final static float EDIT_LINE_WIDTH = 10;
     private final static float NORMAL_LINE_WIDTH = 10;
     private final static int EDIT_COLOR = Color.WHITE;
+    private final static int MAX_UNION_TRIES = 10;
 
 
     public static void setEditableFeature(GeoJsonFeature feature) {
@@ -47,17 +52,57 @@ public class GeometryUtils {
     }
 
     public static GeometryCollection union(GeometryCollection geometryCollection) {
-        ArrayList<Geometry> unionList = unionRec(geometryCollection);
-        if (unionList == null) {
-            return geometryCollection;
-        }
-        GeometryCollection collection = new GeometryFactory().createGeometryCollection(unionList.toArray(new Geometry[unionList.size()]));
-        while (selfIntersection(unionList)) {
-            unionList = unionRec(collection);
-        }
-        collection = new GeometryFactory().createGeometryCollection(unionList.toArray(new Geometry[unionList.size()]));
-        collection = repairGeometryCollection(collection);
+        Geometry geometry;
+        GeometryCollection collection = geometryCollection;
+        int unionTries = 0;
+        do {
+            try {
+                geometry = collection.union();
+            } catch (TopologyException e) {
+                return collection;
+            }
+            if (geometry instanceof Polygonal) {
+                List<Polygon> polygons = null;
+                if (geometry instanceof Polygon) {
+                    polygons = PolygonRepairerService.repair((Polygon) geometry);
+                } else if (geometry instanceof MultiPolygon) {
+                    polygons = PolygonRepairerService.repair((MultiPolygon) geometry);
+                }
+                collection = new GeometryFactory().createGeometryCollection(polygons.toArray(new Polygon[polygons.size()]));
+            } else if (geometry instanceof GeometryCollection) {
+                collection = repairGeometryCollection(((GeometryCollection) geometry));
+            }
+            collection = removeHoles(collection);
+            unionTries++;
+        } while (selfIntersection(collection) && unionTries < MAX_UNION_TRIES);
         return collection;
+
+    }
+
+
+    private static boolean selfIntersection(GeometryCollection geometryCollection) {
+        for (int i = 0; i < geometryCollection.getNumGeometries(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (!geometryCollection.getGeometryN(i).disjoint(geometryCollection.getGeometryN(j))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static GeometryCollection removeHoles(GeometryCollection geometryCollection) {
+        ArrayList<Geometry> newGeometryList = new ArrayList<>();
+        for (int i = 0; i < geometryCollection.getNumGeometries(); i++) {
+            if (geometryCollection.getGeometryN(i) instanceof Polygon && ((Polygon) geometryCollection.getGeometryN(i)).getNumInteriorRing() > 0) {
+                Coordinate[] coordinates = ((Polygon) geometryCollection.getGeometryN(i)).getExteriorRing().getCoordinates();
+                LinearRing linearRing = new GeometryFactory().createLinearRing(coordinates);
+                newGeometryList.add(new GeometryFactory().createPolygon(linearRing));
+            } else {
+                newGeometryList.add(geometryCollection.getGeometryN(i));
+            }
+        }
+        return new GeometryFactory().createGeometryCollection(newGeometryList.toArray(new Geometry[newGeometryList.size()]));
     }
 
     private static GeometryCollection repairGeometryCollection(GeometryCollection collection) {
@@ -74,53 +119,5 @@ public class GeometryUtils {
         return new GeometryFactory().createGeometryCollection(geometries.toArray(new Geometry[geometries.size()]));
     }
 
-    private static ArrayList<Geometry> unionRec(GeometryCollection geometryCollection) {
-        ArrayList<Geometry> geometries = new ArrayList<>();
-        if (geometryCollection.getNumGeometries() == 1) {
-            return null;
-        }
-        for (int i = 0; i < geometryCollection.getNumGeometries(); i++) {
-            boolean intersected = false;
-            for (int j = 0; j < geometryCollection.getNumGeometries(); j++) {
-                if (i != j) {
-                    try {
-                        if (geometryCollection.getGeometryN(i).intersects(geometryCollection.getGeometryN(j))) {
-                            Geometry unionGeometry = geometryCollection.getGeometryN(i).union(geometryCollection.getGeometryN(j));
-                            if (!inList(geometries, unionGeometry)) {
-                                geometries.add(geometryCollection.getGeometryN(i).union(geometryCollection.getGeometryN(j)));
-                            }
-                            intersected = true;
-                        }
-                    } catch (TopologyException e) {
-                    }
-                }
-            }
-            if (!intersected) {
-                if (!inList(geometries, geometryCollection.getGeometryN(i))) {
-                    geometries.add(geometryCollection.getGeometryN(i));
-                }
-            }
-        }
-        return geometries;
-    }
 
-    private static boolean inList(ArrayList<Geometry> geometries, Geometry geometry) {
-        for (int i = 0; i < geometries.size(); i++) {
-            if (geometries.get(i).equalsTopo(geometry)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean selfIntersection(ArrayList<Geometry> geometries) {
-        for (int i = 0; i < geometries.size(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (!geometries.get(i).disjoint(geometries.get(j))) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
 }
