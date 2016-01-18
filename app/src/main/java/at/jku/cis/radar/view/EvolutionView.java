296f803cc1cc451ef3e12b7973a9ec3643599c68
Markus <Markus.Weissenbek@gmx.at>
2016-01-18 12:25:51 +0100

    started coloring

--- app/src/main/java/at/jku/cis/radar/view/EvolutionView.java
+++ app/src/main/java/at/jku/cis/radar/view/EvolutionView.java
@@ -1,19 +1,23 @@
 package at.jku.cis.radar.view;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
+import android.view.ViewParent;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.maps.android.geojson.GeoJsonFeature;
 import com.google.maps.android.geojson.GeoJsonLayer;
+import com.google.maps.android.geojson.GeoJsonPolygonStyle;
 
 import org.json.JSONObject;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+import at.jku.cis.radar.activity.EvolutionActivity;
 import at.jku.cis.radar.model.Event;
 import at.jku.cis.radar.task.GetFeaturesEvolutionTask;
 
@@ -36,9 +40,19 @@ public class EvolutionView extends MapView implements OnMapReadyCallback {
         this.googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         this.geoJsonLayer = new GeoJsonLayer(googleMap, new JSONObject());
         this.geoJsonLayer.addLayerToMap();
+        Event event =(Event) ((EvolutionActivity)getContext()).getIntent().getExtras().get("event");
 
+        int color = event.getColor();
         List<GeoJsonFeature> geoJsonFeatures = loadGeoJsonFeatures();
+        GeoJsonPolygonStyle style;
         for (GeoJsonFeature geoJsonFeature : geoJsonFeatures) {
+            //Color.RED;
+            style = new GeoJsonPolygonStyle();
+            style.setFillColor(color);
+            style.setStrokeColor(Color.WHITE);
+            geoJsonFeature.setPolygonStyle(style);
+            color =  color - 0x000F0F0F;
+            color = color | 0xFF000000;
             geoJsonLayer.addFeature(geoJsonFeature);
         }
     }
